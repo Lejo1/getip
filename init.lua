005a8e9e780315f504df0122d60d72b237db8ccf
Lejo1 <Lejo1@users.noreply.github.com>
2017-07-20 21:22:57 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -28,6 +28,7 @@ minetest.register_chatcommand("getip", {
 			minetest.chat_send_player(name, "The Player is not online")
 			if ip[player] then
 				minetest.chat_send_player(name, "His last ip was: \""..ip[player].."\".")
+			else minetest.chat_send_player(name, "There is no playerip saved.")
 			end
 		else	
 			minetest.chat_send_player(name, "The IP is \""..minetest.get_player_ip(player).."\".")
